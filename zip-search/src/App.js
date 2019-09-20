d9f2bd10c6d8126a79525ed8320843bf1b123ade
Mark Goldstein <markgoldstein12@gmail.com>
2019-09-20 08:16:36 -0400

    added api

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -3,12 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
+  
 function City(props) {
-  return (<div></div>);
+
+  return (<div>
+    
+    </div>);
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+
+  const getZipData = async (zip) => {
+    let res = await fetch(`http://ctp-zip-api.herokuapp.com/zip/${zip}`)
+    let data = await res.json()
+    console.log(data) // Grab the first item in the list JSON response
+    return data;
+  }
+  let zip = props.zip
+  let resApi = getZipData(zip)
+  // const {}
+  console.log(resApi[0])
+  return (<div>
+   
+  </div>);
 }
 
 
@@ -19,7 +37,7 @@ class App extends Component {
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField zip = {props.zip} />
         <div>
           <City />
           <City />
